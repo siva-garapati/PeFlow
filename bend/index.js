6d7aeaec81d61e9a65c87d4958c1f897ff1c8d3f
siva-garapati <sivagarapati999@gmail.com>
2025-10-07 12:26:18 +0530

    fixed small bug

--- bend/index.js
+++ bend/index.js
@@ -10,7 +10,7 @@ let app=express()
 app.use(express.json())
 
 app.use(cors({
-    origin: process.env.CLIENT_ORIGIN_DEPLOYMENT,
+    origin: process.env.CLIENT_ORIGIN,
     credentials: true
 }));
 
